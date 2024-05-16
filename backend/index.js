5d882bd75e80be5f5e1d920243deacfc49958924
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2024-05-17 01:43:37 +0530

    Add PUT method and a GET method to retrieve a book by ID

--- backend/index.js
+++ backend/index.js
@@ -41,10 +41,48 @@ app.post("/books", async (request, response) => {
 app.get("/books", async (request, response) => {
 	try {
 		const books = await Book.find();
-		return response.status(200).json(books);
+		return response.status(200).json({
+			count: books.length,
+			data: books,
+		});
 	} catch (error) {
 		console.log(error.message);
 		response.status(500).send({ message: error.message });
+	}
+});
+
+//get a book by id
+app.get("/books/:id", async (request, response) => {
+	try {
+		const { id } = request.params;
+		const book = await Book.findById(id);
+		return response.status(200).json(book);
+	} catch (error) {
+		console.log(error.message);
+		response.status(500).send({ message: error.message });
+	}
+});
+
+app.put("/books/:id", async (request, response) => {
+	try {
+		if (
+			!request.body.title ||
+			!request.body.author ||
+			!request.body.publishYear
+		) {
+			return response.status(400).send({
+				message: "Send all required fields: title, author, publishYear",
+			});
+		}
+		const { id } = request.params;
+		const result = await Book.findByIdAndUpdate(id, request.body);
+		if (!result) {
+			return response.status(404).send({ message: "Book not found" });
+		}
+		return response.status(200).send({ message: "Book updated successfully" });
+	} catch (error) {
+		console.log(error.message);
+		return response.status(500).send({ message: "Invalid ID" });
 	}
 });
 
