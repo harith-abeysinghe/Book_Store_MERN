9a01ece2b4fd5cfc17d82f7f1e8a232e0498dbd8
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2024-05-17 01:06:50 +0530

    Add POST Method to add a book to database, Add GET method to retrieve all books

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,4 @@
 export const PORT = 1234;
 
-export const mongoBDURL = "mongodb+srv://harithabeysinghe:harith101@bookstoremern.22e9uty.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStoreMERN";
+export const mongoBDURL =
+	"mongodb+srv://harithabeysinghe:harith101@bookstoremern.22e9uty.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStoreMERN";
--- backend/index.js
+++ backend/index.js
@@ -5,43 +5,57 @@ import { Book } from "./models/bookModel.js";
 
 const app = express();
 
-app.get('/', (request,response) =>{
-  console.log(request)
-  return response.status(234).send("Hello")
+app.use(express.json());
+
+app.get("/", (request, response) => {
+	console.log(request);
+	return response.status(234).send("Hello");
 });
 
-app.post('/books',async (request,response) =>{
-  try{
-    if(
-        !request.body.title||
-        !request.body.author||
-        !request.body.publishYear
-    ){
-      return response.status(400).send({
-          message : "Send all required fields: title, author, publishYear",
-      });
-    }
-    const newBooks = {
-      
-    }
+//Method to post a book
+app.post("/books", async (request, response) => {
+	try {
+		if (
+			!request.body.title ||
+			!request.body.author ||
+			!request.body.publishYear
+		) {
+			return response.status(400).send({
+				message: "Send all required fields: title, author, publishYear",
+			});
+		}
+		const newBook = {
+			title: request.body.title,
+			author: request.body.author,
+			publishYear: request.body.publishYear,
+		};
+		const book = await Book.create(newBook);
+		return response.status(201).send(book);
+	} catch (error) {
+		console.log(error.message);
+		response.status(500).send({ message: error.message });
+	}
+});
 
-  }
-  catch(error){
-    console.log(error.message);
-    response.status(500).send({message: error.message})
-  }
-
+//get all books
+app.get("/books", async (request, response) => {
+	try {
+		const books = await Book.find();
+		return response.status(200).json(books);
+	} catch (error) {
+		console.log(error.message);
+		response.status(500).send({ message: error.message });
+	}
 });
 
 mongoose
-  .connect(mongoBDURL)
-  .then(() => {
-    console.log("App connected to database");
-    app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
-
-  })
-  .catch((error) => {
-    console.log(error);
-  })
+	.connect(mongoBDURL)
+	.then(() => {
+		console.log("App connected to database");
+		app.listen(PORT, () => {
+			console.log(`Server is running on http://localhost:${PORT}`);
+		});
+	})
+	.catch((error) => {
+		console.log(error);
+	});
